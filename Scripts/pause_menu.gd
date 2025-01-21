fc1bd921ac377b5ecbd32d357e09b37ff5192ce8
Jayden <jaydenbeach16@gmail.com>
2025-01-21 22:19:10 +0000

    Fixed Pause Menu Bug
    Fixed bug in pause menu where the player could click the menu buttons even though they were invisible

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,14 +1,24 @@
 extends Control
 
+func _ready() -> void:
+	#ensure both pause menu and buttons are fully reset when scene loads
+	set_menu_buttons_enabled(false) # disable all buttons by default
+	$AnimationPlayer.stop() #stop all animations
+	visible = false # hide pause menu initially
+
 #Resumes game by unpausing
 func resume():
 	get_tree().paused = false #sets pause to false
 	$AnimationPlayer.play_backwards("blur") #plays blur animation in reverse
+	set_menu_buttons_enabled(false)
+	visible = false
 
 #pauses the game
 func pause():
 	get_tree().paused = true #sets pause to true
 	$AnimationPlayer.play("blur") #plays blur animation
+	set_menu_buttons_enabled(true)
+	visible = true # show pause menu
 
 #Handling of ESC key press
 func testESC():
@@ -27,7 +37,16 @@ func _on_quit_pressed() -> void:
 
 #restart button press - restarts game
 func _on_restart_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/baseScene.tscn")
+	get_tree().paused = false # unpause game
+	set_menu_buttons_enabled(false) # disable buttons
+	$AnimationPlayer.stop() #stop any ongoing animations
+	get_tree().change_scene_to_file("res://Scenes/baseScene.tscn") # change scene to starting scene
+
+#enable or disable menu buttons
+func set_menu_buttons_enabled(enabled: bool) -> void:
+	$PanelContainer/VBoxContainer/Resume.disabled = !enabled
+	$PanelContainer/VBoxContainer/Restart.disabled = !enabled
+	$PanelContainer/VBoxContainer/Quit.disabled = !enabled
 
 
 func _process(delta):
