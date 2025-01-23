ca8f04cfc97a4355323d522a9eff7d153c8cf2ce
RyanMcC0 <94525660+RyanMcC0@users.noreply.github.com>
2025-01-23 23:15:00 +0000

    added camera and camera following with limits as to not exceed map (#20)

--- Scripts/health_ui.gd
+++ Scripts/health_ui.gd
@@ -5,7 +5,7 @@ extends CanvasLayer
 
 func _ready() -> void:
 	# Find the player node and connect the health_changed signal
-	player = get_parent().get_parent().get_node("Player")  # Adjust the path to your player node
+	player = get_parent().get_parent().get_parent().get_parent().get_node("Player")  # Adjust the path to your player node
 	player.connect("health_changed", Callable(self, "_update_health"))  # Use correct callable
 	_update_health(player.current_health)  # Initialize the UI with current health
 
