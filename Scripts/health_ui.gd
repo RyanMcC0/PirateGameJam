216d4f17bca0914051c22947d7be86a926c3f771
Jayden <jaydenbeach16@gmail.com>
2025-01-23 23:06:58 +0000

    Added increase health upgrade

--- Scripts/health_ui.gd
+++ Scripts/health_ui.gd
@@ -1,13 +1,3 @@
 extends CanvasLayer
 
 @onready var health_label = $HBoxContainer/Label #reference the label for the health 'x3,x2 etc
-@onready var player = null # reference to the player node
-
-func _ready() -> void:
-	# Find the player node and connect the health_changed signal
-	player = get_parent().get_parent().get_node("Player")  # Adjust the path to your player node
-	player.connect("health_changed", Callable(self, "_update_health"))  # Use correct callable
-	_update_health(player.current_health)  # Initialize the UI with current health
-
-func _update_health(new_health: int) -> void:
-	health_label.text = "x" + str(new_health) # update text label
