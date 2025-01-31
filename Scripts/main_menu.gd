3d2090843bde768b85966299243f6b0037c00492
Jayden <jaydenbeach16@gmail.com>
2025-01-31 02:24:41 +0000

    Added upgrade cards and menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,20 +1,21 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var start_game_card = $VBoxContainer/StartGameCard
+@onready var options_card = $VBoxContainer/OptionsCard
+@onready var quit_card = $VBoxContainer/QuitCard
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	start_game_card.pressed.connect(_on_start_game_card_pressed)
+	options_card.pressed.connect(_on_options_card_pressed)
+	quit_card.pressed.connect(_on_quit_card_pressed)
 
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/baseScene.tscn")
-
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
+func _on_start_game_card_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/baseScene.tscn")  # Change to your game scene
 
 
-func _on_controls_button_pressed() -> void:
+func _on_options_card_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/OptionsMenu.tscn")
+
+
+func _on_quit_card_pressed() -> void:
+	get_tree().quit()
--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -1,15 +1,31 @@
 extends Control
 
+@onready var music_slider = $MusicVolumeSliderContainer/MusicVolumeSlider
+@onready var game_slider = $GameVolumeSliderContainer2/GameVolumeSlider
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	music_slider.value = get_bus_volume("Music")
+	game_slider.value = get_bus_volume("SFX")
+	
+	music_slider.connect("value_changed", Callable(self, "_on_music_volume_changed"))
+	game_slider.connect("value_changed", Callable(self, "_on_game_volume_changed"))
 
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+func _on_music_volume_slider_value_changed(value: float) -> void:
+	set_bus_volume("Music", value)
+
+
+func _on_game_volume_slider_value_changed(value: float) -> void:
+	set_bus_volume("SFX", value)
+
+
+func set_bus_volume(bus_name, value):
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+
+func get_bus_volume(bus_name):
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	return db_to_linear(AudioServer.get_bus_volume_db(bus_index))
